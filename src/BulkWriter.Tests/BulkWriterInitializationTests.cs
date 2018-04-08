182b0d3c66f6e701a87101b318e33f3bb808a6e0
Stian Kroknes <stian.kroknes@gmail.com>
2018-04-08 10:05:00 +0200

    Minor improvements fro BulkCopySetup tests

--- src/BulkWriter.Tests/BulkWriterInitializationTests.cs
+++ src/BulkWriter.Tests/BulkWriterInitializationTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Data.SqlClient;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -9,7 +8,6 @@ namespace BulkWriter.Tests
     public class BulkWriterInitializationTests
     {
         private readonly string _connectionString = TestHelpers.ConnectionString;
-        private readonly string _tableName = nameof(BulkWriterInitializationTestsMyTestClass);
 
         public class BulkWriterInitializationTestsMyTestClass
         {
@@ -18,23 +16,22 @@ namespace BulkWriter.Tests
             public string Name { get; set; }
         }
 
-        public BulkWriterInitializationTests()
+        [Table("TestClass2")]
+        public class BulkWriterInitializationTestsMyTestClassAnnotation
         {
-            TestHelpers.ExecuteNonQuery(_connectionString, $"DROP TABLE IF EXISTS [dbo].[{_tableName}]");
+            public int Id { get; set; }
 
-            TestHelpers.ExecuteNonQuery(_connectionString,
-                "CREATE TABLE [dbo].[" + _tableName + "](" +
-                "[Id] [int] IDENTITY(1,1) NOT NULL," +
-                "[Name] [nvarchar](50) NULL," +
-                "CONSTRAINT [PK_" + _tableName + "] PRIMARY KEY CLUSTERED ([Id] ASC)" +
-                ")");
+            public string Name { get; set; }
         }
 
         [Fact]
         public async Task CanSetBulkCopyParameters()
         {
+            string tableName = DropCreate(nameof(BulkWriterInitializationTestsMyTestClass));
+
             const int timeout = 10;
             const int batchSize = 1000;
+            bool setupCallbackInvoked = false;
 
             var writer = new BulkWriter<BulkWriterInitializationTestsMyTestClass>(_connectionString)
             {
@@ -42,19 +39,68 @@ namespace BulkWriter.Tests
                 BatchSize = batchSize,
                 BulkCopySetup = bcp =>
                 {
+                    setupCallbackInvoked = true;
+                    Assert.Equal(nameof(BulkWriterInitializationTestsMyTestClass), bcp.DestinationTableName);
                     Assert.Equal(timeout, bcp.BulkCopyTimeout);
                     Assert.Equal(batchSize, bcp.BatchSize);
                 }
             };
 
             var items = Enumerable.Range(1, 10)
-                .Select(i => new BulkWriterInitializationTestsMyTestClass {Id = i, Name = "Bob"});
+                .Select(i => new BulkWriterInitializationTestsMyTestClass { Id = i, Name = "Bob" });
 
             writer.WriteToDatabase(items);
 
-            var count = (int) await TestHelpers.ExecuteScalar(_connectionString, $"SELECT COUNT(1) FROM {_tableName}");
+            var count = (int)await TestHelpers.ExecuteScalar(_connectionString, $"SELECT COUNT(1) FROM {tableName}");
 
             Assert.Equal(10, count);
+            Assert.True(setupCallbackInvoked);
+        }
+
+        [Fact]
+        public async Task CanSetBulkCopyParameters_Respects_Table_Annotation()
+        {
+            string tableName = DropCreate("TestClass2");
+            const int timeout = 10;
+            const int batchSize = 1000;
+            bool setupCallbackInvoked = false;
+
+            var writer = new BulkWriter<BulkWriterInitializationTestsMyTestClassAnnotation>(_connectionString)
+            {
+                BulkCopyTimeout = timeout,
+                BatchSize = batchSize,
+                BulkCopySetup = bcp =>
+                {
+                    setupCallbackInvoked = true;
+                    Assert.Equal("TestClass2", bcp.DestinationTableName);
+                    Assert.Equal(timeout, bcp.BulkCopyTimeout);
+                    Assert.Equal(batchSize, bcp.BatchSize);
+                }
+            };
+
+            var items = Enumerable.Range(1, 10)
+                .Select(i => new BulkWriterInitializationTestsMyTestClassAnnotation { Id = i, Name = "Bob" });
+
+            writer.WriteToDatabase(items);
+
+            var count = (int)await TestHelpers.ExecuteScalar(_connectionString, $"SELECT COUNT(1) FROM {tableName }");
+
+            Assert.Equal(10, count);
+            Assert.True(setupCallbackInvoked);
+        }
+
+        private string DropCreate(string tableName)
+        {
+            TestHelpers.ExecuteNonQuery(_connectionString, $"DROP TABLE IF EXISTS [dbo].[{tableName}]");
+
+            TestHelpers.ExecuteNonQuery(_connectionString,
+                "CREATE TABLE [dbo].[" + tableName + "](" +
+                "[Id] [int] IDENTITY(1,1) NOT NULL," +
+                "[Name] [nvarchar](50) NULL," +
+                "CONSTRAINT [PK_" + tableName + "] PRIMARY KEY CLUSTERED ([Id] ASC)" +
+                ")");
+
+            return tableName;
         }
     }
 }
