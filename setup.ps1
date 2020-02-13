a687a01695ad279a93fa764d38c99a8c9735e8da
sasha412 <sashank.bandhakavi@gmail.com>
2020-02-13 08:22:51 -0600

    Added first time run script

---
+++ setup.ps1
@@ -0,0 +1,7 @@
+# set PSGallery as trusted so we can install packages from there
+Write-Host 'Trusting PS Gallery'
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+ 
+# Install PSAKE
+Write-Host 'Installing PSake'
+Install-Module -Name psake -Scope CurrentUser
