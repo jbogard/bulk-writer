8fc8809dc915dec6deb72fac22caf27e22fbf862
sasha412 <sashank.bandhakavi@gmail.com>
2020-02-13 17:51:15 -0600

    Fix psake.cmd file
    added a missed braces.

--- psake.cmd
+++ psake.cmd
@@ -1,4 +1,2 @@
 @echo Executing psake build
-rem Helper script for those who want to run psake from cmd.exe
-
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "& {Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted; Install-Module -Force -Name psake -Scope CurrentUser; invoke-psake %*; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "& {Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted; Install-Module -Force -Name psake -Scope CurrentUser; invoke-psake %*; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }}"
