06ddb8058538f3badb7b4142a21032c86fe9620f
Dane Schilling <dane.schilling@headspring.com>
2020-04-02 18:48:03 -0500

    Trivial: Replace tabs with spaces

--- psake-build-helpers.ps1
+++ psake-build-helpers.ps1
@@ -5,7 +5,7 @@ function Set-Project-Properties($version) {
 <Project>
     <PropertyGroup>
         <Version>$version</Version>
-		<Copyright>$(get-copyright)</Copyright>
+        <Copyright>$(get-copyright)</Copyright>
     </PropertyGroup>
 </Project>
 "@
