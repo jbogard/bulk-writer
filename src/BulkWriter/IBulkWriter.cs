a8adf69239066dfa1bc36c22d98a9953538cd72a
Jimmy Bogard <c_jbogard@healthequity.com>
2021-05-12 13:02:38 -0500

    Adding cancellation tokens

--- src/BulkWriter/IBulkWriter.cs
+++ src/BulkWriter/IBulkWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BulkWriter
@@ -11,13 +12,14 @@ namespace BulkWriter
         /// </summary>
         /// <param name="items">Items to load to the database</param>
         void WriteToDatabase(IEnumerable<TResult> items);
-        
+
         /// <summary>
         /// Bulk loads an input enumerable of type <typeparamref name="TResult"/>
         /// </summary>
         /// <param name="items">Items to load to the database</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
         /// <returns>Awaitable task for writing to the database</returns>
-        Task WriteToDatabaseAsync(IEnumerable<TResult> items);
+        Task WriteToDatabaseAsync(IEnumerable<TResult> items, CancellationToken cancellationToken = default);
 
 #if NETSTANDARD2_1
 
@@ -25,8 +27,9 @@ namespace BulkWriter
         /// Bulk loads an input async enumerable of type <typeparamref name="TResult"/>
         /// </summary>
         /// <param name="items">Items to async load to the database</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
         /// <returns>Awaitable task for writing to the database</returns>
-        Task WriteToDatabaseAsync(IAsyncEnumerable<TResult> items);
+        Task WriteToDatabaseAsync(IAsyncEnumerable<TResult> items, CancellationToken cancellationToken = default);
 #endif
     }
 }
